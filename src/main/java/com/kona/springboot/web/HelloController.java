d0718972f28f892a12e64830d7996f20df501372
jk0908 <biblebest@naver.com>
2020-01-30 21:16:58 +0900

    작업한거 github에 올리기

--- src/main/java/com/kona/springboot/web/HelloController.java
+++ src/main/java/com/kona/springboot/web/HelloController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class HelloController {
 
-    @GetMapping("/hello/dto")
+    @GetMapping("/hello")
     public String hello() {
+
         return "hello";
     }
 
+    @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
         return new HelloResponseDto(name, amount);
     }
